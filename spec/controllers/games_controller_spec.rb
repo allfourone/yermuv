94227d3326840835147aa18f19e74a75f5b0d5ae
Tyler Porter <tyler.b.porter@gmail.com>
2019-07-15 00:43:22 +0000

    Imported factory_bot_rails

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe GamesController, type: :controller do
 	describe "games#show action" do 
 
 		it "should succesfully show the page if the game is found" do 
-game = FactoryBot.create(:game)
+			game = FactoryBot.create(:game)
 			get :show, params: {id: game.id  }
 			expect(response).to have_http_status(:success)
 		end
