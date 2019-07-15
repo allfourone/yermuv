5f9eb22f7fa346cfc12b4afcd0809936fdbe48bc
Wilson Horrell <wilsonhorrell@gmai.com>
2019-07-15 00:18:31 +0000

    Having a problem with creating an object using Capybara

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ApplicationRecord
+
+	validates :name, presence: true 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+
+  resources :games, only: [:new, :create, :show]
 end
