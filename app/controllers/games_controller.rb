26ec81095fe83ca91341b6b8ad3a0ef6394d9ed8
Wilson Horrell <wilsonhorrell@gmai.com>
2019-07-16 14:50:32 +0000

    Forgot to address games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class GamesController < ApplicationController
-<<<<<<< HEAD
+
   before_action :authenticate_user!, only: [:new, :create]
   def new
     @game = Game.new
@@ -11,15 +11,7 @@ class GamesController < ApplicationController
     @game = Game.create(game_params)
     redirect_to root_path
 
-=======
-  def new
-    @game = Game.new
-  end
 
-  def create
-    @game = Game.create(game_params)
-    redirect_to root_path
->>>>>>> master
   end
 
   def show
