6ac39c693133871e88767a2ced259a12acd26d27
Tyler Porter <tyler.b.porter@gmail.com>
2019-07-19 01:56:28 +0000

    Fixed bug not allowing 2nd move

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -8,6 +8,9 @@ $(() => {
 		drop: (e, ui) => {
 			e.target.innerHTML = `<p class="piece">${ui.draggable.html()}</p>`;
 			ui.draggable.remove();
+			$('.piece').draggable({
+				revert: "invalid"
+			});
 		}
 	});
 
