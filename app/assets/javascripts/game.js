e39053f75912a4ea64b6735d2ee2a17e45c94074
Tyler Porter <tyler.b.porter@gmail.com>
2019-07-19 01:53:08 +0000

    Imported jQueryUI and set up drag and drop

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,1 +1,14 @@
-console.log("working");
+$(() => {
+
+	$('.piece').draggable({ 
+		revert: "invalid"
+	});
+
+	$('.col-1').droppable({
+		drop: (e, ui) => {
+			e.target.innerHTML = `<p class="piece">${ui.draggable.html()}</p>`;
+			ui.draggable.remove();
+		}
+	});
+
+});
