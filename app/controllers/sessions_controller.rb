4880e58b7c1ee3f45ca0437e6e3ae3b0d613f887
drew <drewmyerssss@gmail.com>
2019-07-20 16:58:21 +0000

    verification needed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
     def create
-        @user = User.find_or_create_from_auth_hash(env["omniauth.auth"])
+        @user = User.find_or_create_from_auth_hash(ENV['omniauth.auth'])
         sessions[:user_id] = @user.id
         redirect_to :me
     end
