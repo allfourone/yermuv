044184c54379375614ededcd8016c9131b20bf9a
Tyler Porter <tyler.b.porter@gmail.com>
2019-07-21 18:02:54 +0000

    Added validation for white pawns

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -2,33 +2,55 @@ $(() => {
 
 	// This needs to be an AJAX request to pull from the DB and not hard-coded!
 	var state = [
-		['br','bn','bb','bq','bk','bb','bn','br'],
-		['bp','bp','bp','bp','bp','bp','bp','bp'],
-		[null,null,null,null,null,null,null,null],
-		[null,null,null,null,null,null,null,null],
-		[null,null,null,null,null,null,null,null],
-		[null,null,null,null,null,null,null,null],
-		['wp','wp','wp','wp','wp','wp','wp','wp'],
-		['wr','wn','wb','wq','wk','wb','wn','wr']	
+			['&#9823;','&#9822;','&#9821;','&#9819;','&#9818;','&#9821;','&#9822;','&#9823;'],
+			['&#9823;','&#9823;','&#9823;','&#9823;','&#9823;','&#9823;','&#9823;','&#9823;'],
+			[null,null,null,null,null,null,null,null],
+			[null,null,null,null,null,null,null,null],
+			[null,null,null,null,null,null,null,null],
+			[null,null,null,null,null,null,null,null],
+			['&#9817;','&#9817;','&#9817;','&#9817;','&#9817;','&#9817;','&#9817;','&#9817;'],
+			['&#9814;','&#9816;','&#9815;','&#9813;','&#9812;','&#9815;','&#9816;','&#9814;']	
 	];
 
 	$('.piece').draggable({ 
 		revert: "invalid"
 	});
 
+	let validatePawn = (piece, destination) => {
+		let originX = parseInt($(piece).parent().attr('col'));
+		let originY = parseInt($(piece).parent().attr('row'));
+		let destX = parseInt($(destination).attr('col'));
+		let destY = parseInt($(destination).attr('row'));
+
+		if (originX !== destX) {
+			return false;
+		}
+
+		if (originY == 6 && (destY == 5 || destY == 4)) {
+			return true;		
+		} else if (destY > 4) {
+			return false;
+		}
+
+		if (originY - 1 == destY) {
+			return true;
+		}
+		
+		return false;
+	}
+
 	$('.col-1').droppable({
 		drop: (e, ui) => {
-			validatePawn(ui.draggable, state);
-			e.target.innerHTML = `<p class="piece">${ui.draggable.html()}</p>`;
-			ui.draggable.remove();
-			$('.piece').draggable({
-				revert: "invalid"
-			});
+			if (validatePawn(ui.draggable, e.target)) {
+				e.target.innerHTML = `<p class="piece">${ui.draggable.html()}</p>`;
+				ui.draggable.remove();
+				$('.piece').draggable({
+					revert: "invalid"
+				});				
+			}	else {
+				return $(ui.draggable).draggable("option", "revert", true);
+			}		
 		}
 	});
-
-	let validatePawn = (piece) => {
-		// Implement me!
-	}
 
 });
