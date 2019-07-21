c6a6c41208ac4750b6019ae2f8573c364db3311d
Wilson Horrell <wilsonhorrell@gmai.com>
2019-07-21 19:11:03 +0000

    Attempting to add Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -17,18 +17,10 @@ ActiveRecord::Schema.define(version: 2019_07_14_184152) do
 
   create_table "games", force: :cascade do |t|
     t.string "name"
-<<<<<<< HEAD
-
-=======
->>>>>>> master
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_games_on_user_id"
-<<<<<<< HEAD
-
-=======
->>>>>>> master
   end
 
   create_table "users", force: :cascade do |t|
