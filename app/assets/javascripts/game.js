35187198541a07f6cfdf6dd87bfce3fdfca8dbd7
Tyler Porter <tyler.b.porter@gmail.com>
2019-07-21 19:09:10 +0000

    Added comments to help parse functions

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -16,25 +16,30 @@ $(() => {
 		revert: "invalid"
 	});
 
+	// Returns true if the squares are occupied by same color
+	// Else, it returns an array with the piece colors (or empty) 
 	let squareOccupiedBySameColor = (piece, destination) => {
 		let pieceColor = piece.html().charCodeAt(0) < 9818 ? "white" : "black";
 		let destinationPiece = destination.innerText ? destination.innerText.charCodeAt(0) : false;
 		console.log(destination.innerText,destinationPiece)
 		let destinationColor = !destinationPiece ? "empty" : destinationPiece < 9818 ? "white" : "black";
 		
-		return pieceColor === destinationColor ? false : [pieceColor, destinationColor];
+		return pieceColor === destinationColor ? true : [pieceColor, destinationColor];
 	}
 
 	let validatePawn = (piece, destination) => {
+		// Begin by checking if the square is occupied by same color
 		if (!squareOccupiedBySameColor(piece, destination)) {
 			return false;
 		} 
 		
+		// Get the colors of pieces occupying the squares
 		let colors = squareOccupiedBySameColor(piece, destination)
 		let pieceColor = colors[0];
 		let destinationColor = colors[1];
 		console.log(colors, pieceColor, destinationColor)
 
+		// Is only capturable if the destination is not empty and the colors are different
 		let capturable = () => {
 			if (pieceColor !== 'empty' && destinationColor !== 'empty') {
 				if (pieceColor !== destinationColor) {
@@ -44,8 +49,10 @@ $(() => {
 			return false;
 		}
 
+		// Calc if the destination is empty
 		let empty = destinationColor == 'empty';
 
+		// Calculate origin & destination coords given the HTML nodes
 		let originX = parseInt($(piece).parent().attr('col'));
 		let originY = parseInt($(piece).parent().attr('row'));
 		let destX = parseInt($(destination).attr('col'));
@@ -55,12 +62,19 @@ $(() => {
 			return false;
 		}
 
+		// Check if piece has moved yet, and allow either 1 or 2 squares
+		// Only works for white pawns currently
 		if (originY == 6 && (destY == 5 || destY == 4)) {
 			return true;		
 		} else if (destY > 4) {
 			return false;
 		}
 
+		// Check if pawn moving forward and square is empty
+		// If square is empty, move forward in straight line
+		// If square is not empty, stop movement
+		// If moving diagonally, check if the distance between rows is 1
+		// If it is, and the piece is of the opposite color, allow movement
 		if (originY - 1 == destY && (empty || (Math.pow((originX - destX),2) == 1 && capturable))) {
 			console.log(empty, pieceColor, destinationColor)
 			return true;
@@ -69,6 +83,7 @@ $(() => {
 		return false;
 	}
 
+	// jQueryUI drag and drop function
 	$('.col-1').droppable({
 		drop: (e, ui) => {
 			if (validatePawn(ui.draggable, e.target)) {
