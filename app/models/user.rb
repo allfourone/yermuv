3c9ffd39a9f9aee7ae1e85ed817050000959ef76
drew <drewmyerssss@gmail.com>
2019-07-23 18:03:45 +0000

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,15 @@ class User < ApplicationRecord
 
   has_many :games
 
-  def self.find_or_create_from_auth_hash(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name
-      user.email = auth.info.email
-      user.picture = auth.info.image
-      user.save!
-    end
+  def self.from_omniauth(auth)
+    find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
+  end
+
+  def self.create_user_from_omniauth(auth)
+    create(
+      provider: auth['provider'],
+      uid: auth['uid'],
+      name: auth['info']['name']
+    )
   end
 end
