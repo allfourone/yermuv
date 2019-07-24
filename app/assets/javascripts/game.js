690bb7f9d103795d304524a349fd04575ef067b3
Wilson Horrell <wilsonhorrell@gmai.com>
2019-07-24 19:04:02 +0000

    Adding logic to javascript and pieces

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -27,72 +27,110 @@ $(() => {
     }
 
     let validatePawn = (piece, destination) => {
-        // Begin by checking if the square is occupied by same color
-        if (!squareOccupiedBySameColor(piece, destination)) {
-            return false;
+        //WH -switch statement for assigning pieces
+        var chessPieceType = '';
+        var chessPiece = piece.html().charCodeAt(0);
+        switch (chessPiece) {
+            case 9817:
+                chessPieceType = 'pawn';
+                break;
+            case 9814:
+                chessPieceType = 'rook';
+                break;
+            case 9816:
+                chessPieceType = 'knight';
+                break;
+            case 9815:
+                chessPieceType = 'bishop';
+                break;
+            case 9813:
+                chessPieceType = 'queen';
+                break;
+            case 9812:
+                chessPieceType = 'king';
+                break;
         }
+        //WH- assign unicode charcters to specific pieces
+        /*   let pawn = 9817
+           let rook = 9814
+           let knight = 9816
+           let bishop = 9815
+           let queen = 9813
+           let king = 9812 */
 
-        // Get the colors of pieces occupying the squares
-        let colors = squareOccupiedBySameColor(piece, destination)
-        let pieceColor = colors[0];
-        let destinationColor = colors[1];
 
-        // Is only capturable if the destination is not empty and the colors are different
-        let capturable = () => {
-            if (destinationColor !== 'empty' && pieceColor !== destinationColor) {
-                return true;
-            }
-            return false;
-        }
-
-        // Calc if the destination is empty
-        let empty = destinationColor === 'empty';
-
-        // Calculate origin & destination coords given the HTML nodes
-        let originX = parseInt($(piece).parent().attr('col'));
-        let originY = parseInt($(piece).parent().attr('row'));
-        let destX = parseInt($(destination).attr('col'));
-        let destY = parseInt($(destination).attr('row'));
-
-        // Set positive direction for white, negative direction for black
-        let direction = pieceColor === "white" ? 1 : -1;
-
-        // Only allow diagonal movement if a piece is capturable
-        if (originX !== destX && !capturable()) {
-            return false;
-        }
-
-        // Check if piece has moved yet, and allow either 1 or 2 squares
-        // Works for both black and white pawns
-        if (direction > 0) {
-            if (originY == 6 && (destY == 5 || destY == 4)) {
-                return true;
-            } else if (originY == 6 && destY < 4) {
+        //WH- Setting piece rules for pawn piece only
+        if (chessPieceType = 'pawn') {
+            // Begin by checking if the square is occupied by same color
+            if (!squareOccupiedBySameColor(piece, destination)) {
                 return false;
             }
-        } else if (direction < 0) {
-            if (originY == 1 && (destY == 2 || destY == 3)) {
-                return true;
-            } else if (originY == 1 && destY > 3) {
+
+            // Get the colors of pieces occupying the squares
+            let colors = squareOccupiedBySameColor(piece, destination)
+            let pieceColor = colors[0];
+            let destinationColor = colors[1];
+
+            // Is only capturable if the destination is not empty and the colors are different
+            let capturable = () => {
+                if (destinationColor !== 'empty' && pieceColor !== destinationColor) {
+                    return true;
+                }
                 return false;
             }
-        }
 
-        // Check if pawn moving forward based on direction 
-        if (originY - (1 * direction) !== destY) {
+            // Calc if the destination is empty
+            let empty = destinationColor === 'empty';
+
+            // Calculate origin & destination coords given the HTML nodes
+            let originX = parseInt($(piece).parent().attr('col'));
+            let originY = parseInt($(piece).parent().attr('row'));
+            let destX = parseInt($(destination).attr('col'));
+            let destY = parseInt($(destination).attr('row'));
+
+            // Set positive direction for white, negative direction for black
+            let direction = pieceColor === "white" ? 1 : -1;
+
+            // Only allow diagonal movement if a piece is capturable
+            if (originX !== destX && !capturable()) {
+                return false;
+            }
+
+            // Check if piece has moved yet, and allow either 1 or 2 squares
+            // Works for both black and white pawns
+            if (direction > 0) {
+                if (originY == 6 && (destY == 5 || destY == 4)) {
+                    return true;
+                } else if (originY == 6 && destY < 4) {
+                    return false;
+                }
+            } else if (direction < 0) {
+                if (originY == 1 && (destY == 2 || destY == 3)) {
+                    return true;
+                } else if (originY == 1 && destY > 3) {
+                    return false;
+                }
+            }
+
+            // Check if pawn moving forward based on direction 
+            if (originY - (1 * direction) !== destY) {
+                return false;
+            }
+
+            // If square is empty, move forward in straight line
+            // If square is not empty, stop movement
+            // If moving diagonally, check if the distance between rows is 1
+            // If it is, and the piece is of the opposite color, allow movement
+            if ((empty && !capturable()) || (!empty && (Math.pow((originX - destX), 2) == 1 && capturable()))) {
+                return true;
+            }
+
             return false;
         }
 
-        // If square is empty, move forward in straight line
-        // If square is not empty, stop movement
-        // If moving diagonally, check if the distance between rows is 1
-        // If it is, and the piece is of the opposite color, allow movement
-        if ((empty && !capturable()) || (!empty && (Math.pow((originX - destX), 2) == 1 && capturable()))) {
-            return true;
-        }
-
-        return false;
     }
+
+
 
     // jQueryUI drag and drop function
     $('.col-1').droppable({
