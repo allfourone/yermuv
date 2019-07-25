e79c78157fde8fab1c006b1edb5a15727a2a81dc
drew <drewmyerssss@gmail.com>
2019-07-25 20:54:47 +0000

    two logout buttons

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
 
     def create
-        @user = User.find_or_create_from_auth_hash(ENV['omniauth.auth'])
-        sessions[:user_id] = @user.id
+        @user = User.find_or_create_by(ENV['omniauth.auth'])
+        session[:user_id] = @user.id
         redirect_to :me
     end
 
