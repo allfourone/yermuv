8b42c54878c0a4235d081cae48a849c670982122
drew <drewmyerssss@gmail.com>
2019-07-25 15:36:38 +0000

    figaro gem

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, '583359957721-kjnhpctjn68i8609nbod3ilmoj746lr5.apps.googleusercontent.com', 'TfqAqMsj4j4s1a6K5ubXp-gx',
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
       {
         scope: 'userinfo.email, userinfo.profile, http://gdata.youtube.com',
         prompt: 'select_account',
