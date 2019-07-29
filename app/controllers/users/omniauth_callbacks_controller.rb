3e4f1f9eaabf266b952c9463d669e95780233c67
drew <drewmyerssss@gmail.com>
2019-07-29 17:48:16 +0000

    everything working

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
-class OmniauthCallbacksController
-  module Users
-    module Devise
-      def google_oauth2
-        @user = User.from_omniauth(request.env['omniauth.auth'])
-        sign_in_and_redirect @user
-      end
+module Users
+  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def google_oauth2
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+      sign_in_and_redirect @user
     end
   end
 end
