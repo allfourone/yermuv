8bf1c400b19e6ec79a81b22d66f05f2a7ce085fc
Tyler Porter <tyler.b.porter@gmail.com>
2019-07-30 01:53:56 +0000

    Removed Omniauth and reverted to working Devise authentication

--- app/controllers/omniauth_callbacks_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def google_oauth2
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-    sign_in_and_redirect @user
-  end
-end
