2059f063666843656b4931e684305d821dd4a908
Tyler Porter <tyler.b.porter@gmail.com>
2019-08-03 17:06:18 +0000

    Removed prior changes to application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,6 @@ module Yermuv
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Add any keys in application.yml here.
-    Figaro.require_keys("GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET", "HEROKU_API_KEY")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
