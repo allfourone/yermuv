d18fa2fdd059d7ca8df144aa1283b6f7d2c30260
Tyler Porter <tyler.b.porter@gmail.com>
2019-08-05 01:48:10 +0000

    Refactored knight code and fixed bugs

--- app/assets/javascripts/pieces/knight.js
+++ app/assets/javascripts/pieces/knight.js
@@ -24,7 +24,7 @@ let validateKnight = (piece, destination, state) => {
         if (destinationColor !== 'empty' && pieceColor !== destinationColor) {
             return true;
         }
-
+        
         return false;
     }
 
@@ -37,34 +37,15 @@ let validateKnight = (piece, destination, state) => {
     let destX = parseInt($(destination).attr('col'));
     let destY = parseInt($(destination).attr('row'));
 
-    // Make sure it is not moving in a straight line in either direction
-    if (destX === originX || destY === originY) {
-
-        return false;
-    }
-    
-    // Create an array of possible moves for knights given origin coords
-    // 8 possible moves for each knight, but 4 of them are inverses
-    // of the other. Only need to generate 4 unique moves then reverse them.
-    let moveArray = [];
-    moveArray.push([originX - 1, originY - 2]);
-    moveArray.push([originX + 1, originY - 2]);
-    moveArray.push([originX + 2, originY - 1]);
-    moveArray.push([originX + 2, originY + 1]);
-
-    // For each move, generate the inverse and add it to the array of moves
-    for (let i = 0; i < 4; i++) {
-        moveArray.push(moveArray[i].reverse());
-    }
-
-    // Check if the move is possible by iterating over the array
-    // Check if valid X and Y coords match the destination coords, or return false
+    // Check if the move is possible by checking if moving in an L shape
     let movePossible = () => {
-        for (let i = 0; i < moveArray.length; i++) {
-            if (moveArray[i][0] == destY && moveArray[i][1] == destX) {
-                return true;
-            }            
+        let xDif = Math.abs(destX - originX);
+        let yDif = Math.abs(destY - originY);
+
+        if ( (xDif == 2 && yDif == 1) || (xDif == 1 && yDif == 2)) {
+            return true;
         }
+
         return false;
     }
     
